f8e2b6b02c0dbebe5a87c505def28e1ad16be89f
Besher Kitaz <kitazb@berea.edu>
2025-06-24 23:40:10 -0400

    Added my name and worked a little bit on the layout.

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className='appContainer'>
+          <div className='greetContainer'>
+            <p > Hello </p>
+            <p className='iAmContainer'> I am </p>
+            <div className='nameContainer'>
+              <p className='ms-5'> Besher</p>
+              <p className='ms-15'> Kitaz!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
