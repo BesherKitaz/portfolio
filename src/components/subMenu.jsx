d5a5adf9d0134b9226549e256f5e201e019eec8a
Besher Kitaz <kitazb@berea.edu>
2026-02-12 01:31:54 -0500

    Fixed resume link

--- src/components/subMenu.jsx
+++ src/components/subMenu.jsx
@@ -5,7 +5,7 @@ const SubMenu = () => {
         <div className="subMenu">
             <ul>
                 <li> <a href='https://github.com/BesherKitazBerea/portfolio' target="_blank" rel="noopener noreferrer"> Site's Repository </a> </li>
-                <li><Link to="">Download Resume</Link></li>
+                <li><a href="public/Resume - Feb 11th.pdf" download>Download Resume</a></li>
             </ul>
         </div>
     );
