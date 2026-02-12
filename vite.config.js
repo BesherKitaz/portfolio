44891f9d16b21ecdb0db4cc8cc2076870ee05b90
Besher Kitaz <kitazb@berea.edu>
2026-02-12 00:38:49 -0500

    project built

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs', // <-- change "dist" to "build" or whatever you like
+  },
   plugins: [react()],
+  base: "/portfolio/",
 })
